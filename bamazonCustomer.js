ff388176c147da82c90ce7b56fd3fd14a5d514d7
Wayne Baylor <wcsbaylor@gmail.com>
2018-04-18 23:20:00 -0500

    refactored initial search function and inquirer prompt

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -15,7 +16,10 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("up and running");
+    // retrieveProducts();
     retrieveProducts();
+    connection.end();
+    
 });
 
 function retrieveProducts() {
@@ -25,14 +29,43 @@ function retrieveProducts() {
             // Log all results of the SELECT statement
               for (var i = 0; i < res.length; i++) {
             console.log(
-              "item_id: " +
-                res[i].item_id +
-                " || product_name: " +
-                res[i].product_name +
-                " || price: " +
-                res[i].price
+              "item_id: " + res[i].item_id + " || product_name: " + res[i].product_name + res[i].price                               
             );
+            console.log("------------------------------------------------");
           }
-          connection.end();
         });
+        purchaseProduct() 
     }
+
+function purchaseProduct() {
+
+    connection.query("SELECT product_name FROM products", function(err, data){
+        if (err) throw err;
+
+    inquirer
+        .prompt([{
+            name: "select",
+            type: "list",
+            message: "what product would you like to buy?",
+            choices: function() {
+                let choiceArray = [];
+                for (let i = 0; i < data.length; i++) {
+                    choiceArray.push(data[i].product_name)
+                }
+                return choiceArray;
+            }
+        },
+        ]).then(function(answer){
+        inquirer
+            .prompt([{
+                name: "quantity",
+                type: "input",
+                message: "Please input the amount you'd like to purchase of " + answer.select 
+                + ": "
+                //how do I use the 
+            }]).then(function(anotherOne){
+                console.log("things are happening");
+            })
+        });
+    });
+}
