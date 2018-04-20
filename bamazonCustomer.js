bebefe326d2cc727ff244156c818946c7129ba79
Wayne Baylor <wcsbaylor@gmail.com>
2018-04-19 23:07:44 -0500

    created array to reference all data from db. NS: get index of user input and check quantity of item

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,7 +14,9 @@ var connection = mysql.createConnection({
 });
 
 connection.connect(function (err) {
-    if (err) throw err;
+    if (err) {
+        console.log(err)
+    };
     console.log("up and running");
     // retrieveProducts();
     retrieveProducts();
@@ -22,49 +24,76 @@ connection.connect(function (err) {
     
 });
 
+let masterArray = [];
+
 function retrieveProducts() {
     console.log("Selecting all available products...\n");
-    connection.query("SELECT item_id, product_name, price FROM products", function (err, res) {
-            if (err) throw err;
-            // Log all results of the SELECT statement
+    connection.query("SELECT * FROM products", function (err, res) {
+            if (err) {
+                console.log(err)
+            };
+            
               for (var i = 0; i < res.length; i++) {
+                  masterArray.push(res[i]);
             console.log(
               "item_id: " + res[i].item_id + " || product_name: " + res[i].product_name + res[i].price                               
             );
             console.log("------------------------------------------------");
           }
         });
-        purchaseProduct() 
+        purchaseProduct(); 
     }
 
 function purchaseProduct() {
 
-    connection.query("SELECT product_name FROM products", function(err, data){
-        if (err) throw err;
-
+    connection.query("SELECT product_name, stock_quantity FROM products", function(err, data){
+        if (err) {
+            console.log(err);
+        }
+        let choiceArray = [];
+        let choices = function() {for (let i = 0; i < data.length; i++) {
+            choiceArray.push(data[i].product_name)
+        }
+        return choiceArray;
+    }
+    
     inquirer
         .prompt([{
             name: "select",
             type: "list",
             message: "what product would you like to buy?",
-            choices: function() {
-                let choiceArray = [];
-                for (let i = 0; i < data.length; i++) {
-                    choiceArray.push(data[i].product_name)
-                }
-                return choiceArray;
-            }
-        },
+            choices: choices()
+        }
+        ,
         ]).then(function(answer){
         inquirer
             .prompt([{
                 name: "quantity",
                 type: "input",
                 message: "Please input the amount you'd like to purchase of " + answer.select 
-                + ": "
+                + ": ",
+                validate: function(value) {
+                    if (isNaN(value) === false) { 
+                        return true;
+                    }
+                    return false;
+                    }
+                }
                 //how do I use the 
-            }]).then(function(anotherOne){
-                console.log("things are happening");
+            ]).then(function(value){
+                console.log(masterArray);
+                for (i = 0; i < masterArray.length; i++) {
+                    console.log(masterArray[i].product_name);
+                    // if (value.quantity < )
+                }
+                // console.log(stock_quantity);
+                console.log(answer.select);
+                console.log(value.quantity);
+                // let desiredQuant = value.quantity;
+                // if (desiredQuant < stock_quantity) {
+                //     console.log("i")
+                // }
+                // });
             })
         });
     });
